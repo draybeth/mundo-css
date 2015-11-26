b4945dadbf48b75667d99d8f40a81b99b7e5f2af
Santos Avid <apholo_144@hotmail.com>
2015-11-26 16:58:51 -0500

    mundo css3

---
+++ tezla/js/ejemplo.js
@@ -0,0 +1,3 @@
+/**
+ * Created by avid on 10/11/15.
+ */
---
+++ tezla/js/modernizr-custom.js
@@ -0,0 +1,3 @@
+/*! modernizr 3.2.0 (Custom Build) | MIT *
+ * http://modernizr.com/download/?-ambientlight-applicationcache-audio !*/
+!function(n,o,i){var Modernizr=function(){};Modernizr.prototype=ModernizrProto,Modernizr=new Modernizr;Modernizr.addTest("applicationcache","applicationCache"in n)}(window,document);
